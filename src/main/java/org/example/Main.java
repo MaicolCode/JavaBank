1a2d0d6d31b5726a63f990e8b117aed9062048d9
MaicolCode <paucarjhon07@outlook.com>
2025-03-05 19:24:34 -0500

    Actualizando el comentario desde la nueva rama

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, JavaBank ATM!");
+        System.out.println("Hello, JavaBank ATM! with git repository");
     }
 }
