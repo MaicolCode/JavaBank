9bcf3811076379d7fe5199b461030382ba9d285f
MaicolCode <paucarjhon07@outlook.com>
2025-03-05 21:41:09 -0500

    Validacion de pin para el acceso a la cuenta bancaria

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,15 @@ public class Main {
     private String pin;
 
     public static void main(String[] args) {
-
-
         int pin = 1234;
-
+        BankAccount account1 = new BankAccount("123456789", 5000.12);
         AuthenticateUser user = new AuthenticateUser(pin);
 
-        System.out.println(user.authenticateUser("1234"));
-
-
-
-        BankAccount account1 = new BankAccount("123456789", 5000.12);
-        account1.showBalance();
-        account1.deposit(100);
-        account1.withdrawals(12);
+        if(user.authenticateUser("1234")){
+            account1.showBalance();
+            account1.deposit(100);
+            account1.withdrawals(12);
+        }
 
     }
 
