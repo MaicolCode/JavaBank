e45f1e779f8e970b5bf49a61ea6d8e156b7bd7c2
MaicolCode <paucarjhon07@outlook.com>
2025-03-07 21:26:42 -0500

    Se agrega nueva clase no se cual es su funcion :(

--- src/main/java/org/example/SavingsAccount.java
+++ src/main/java/org/example/SavingsAccount.java
@@ -1,4 +1,15 @@
 package org.example;
 
-public class SavingsAccount {
+public class SavingsAccount extends BankAccount {
+    private double interestRate;
+
+    public SavingsAccount(String accountNumber, double balance,double interestRate ){
+        super(accountNumber,balance );
+        this.interestRate = interestRate;
+    }
+
+    public void applyInterest() {
+        double interest = showBalance()*interestRate;
+        deposit(interest);
+    }
 }
