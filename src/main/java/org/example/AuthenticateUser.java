4ffbbf5d8f0b82319455d9b762047268d3f91bb5
MaicolCode <paucarjhon07@outlook.com>
2025-03-07 22:47:42 -0500

    Se modifico la clase y se refactorizo el codigo para la validacion

--- src/main/java/org/example/AuthenticateUser.java
+++ src/main/java/org/example/AuthenticateUser.java
@@ -9,15 +9,7 @@ public class AuthenticateUser {
 
     public boolean authenticateUser(String inputPin) {
         int attempts = 0;
-        while(attempts < 3){
-            if(this.pin == Integer.parseInt(inputPin)){
-                return true;
-            } else {
-                attempts++;
-                System.out.println("Pin incorrecto. Intento "+attempts+" de 3.");
-            }
-        }
-        return false;
+        return this.pin == Integer.parseInt(inputPin);
     }
 
     public void setPin(int pin) {
